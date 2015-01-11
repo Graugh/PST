6a5e467f50086bdf8e4fcd04a9aa0e3803278258
karolgil <karolgil.kg@gmail.com>
2015-01-11 18:22:39 +0100

    Minor refactoring increasing code readability.

--- DataReader/data_reader.py
+++ DataReader/data_reader.py
@@ -8,12 +8,17 @@ class DataReader(object):
     def open_data_file_from_path(self, path):
         self.data_file = open(path, 'r')
 
+    def _search_for_equation_with_given_variable_in_string(self, variable_name, string):
+        return re.search('%s=[\d|\.]*'%variable_name, string)
+
+    def _search_for_value_of_equation(self, equation):
+        return float(re.search('[\d|\.]+$', equation).group(0))
+
     def _get_variable_from_data_file(self, variable_name):
         for line in self.data_file:
-            match_variable = re.search('%s=[\d|\.]*'%variable_name, line)
-            if match_variable:
-                match_value = re.search('[\d|\.]+$', match_variable.group(0))
-                return float(match_value.group(0))
+            match_equation = self._search_for_equation_with_given_variable_in_string(variable_name, line)
+            if match_equation:
+                return self._search_for_value_of_equation(match_equation.group(0))
 
     def create_data_dict(self):
         return {
