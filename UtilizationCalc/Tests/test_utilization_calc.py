db6e175b3cdb67da32026baf7b1a93df32da29dc
karolgil <karolgil.kg@gmail.com>
2015-01-11 17:45:44 +0100

    Added methods for calculating list of radiuses.

--- UtilizationCalc/Tests/test_utilization_calc.py
+++ UtilizationCalc/Tests/test_utilization_calc.py
@@ -11,3 +11,9 @@ def utilization_calc():
                                                                                      [32, 12, 16, 12]])
 def test_calculate_M(utilization_calc, no_of_users, no_of_subbands, no_of_radiuses, expected_M):
     assert expected_M == utilization_calc.calculate_M(no_of_users, no_of_subbands, no_of_radiuses)
+
+
+@pytest.mark.parametrize('delta, no_of_multicasts, expected_radiuses', [[0.2, 3, [0.6, 0.82462, 1]],
+                                                                        [0.5, 4, [0.66144, 0.79057, 0.90139, 1]]])
+def test_calculate_vector_of_radiuses(utilization_calc, delta, no_of_multicasts, expected_radiuses):
+    assert expected_radiuses == utilization_calc.calculate_vector_of_radiuses(delta, no_of_multicasts)
--- UtilizationCalc/utilization_calc.py
+++ UtilizationCalc/utilization_calc.py
@@ -1,6 +1,13 @@
+import math
+
+
 class UtilizationCalc(object):
     def calculate_M(self, no_of_users, no_of_subbands, no_of_radiuses):
         return min(no_of_users, no_of_subbands, no_of_radiuses)
 
-    def calculate_vector_of_radiuses(self):
-        pass
+    def calculate_vector_of_radiuses(self, delta, no_of_multicasts):
+        radiuses = []
+        for m in range(1, no_of_multicasts):
+            radiuses.append(round(math.sqrt(delta * delta + m * ((1 - delta * delta)/no_of_multicasts)), 5))
+        radiuses.append(1)
+        return radiuses
