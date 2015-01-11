2c039784713737c53e0d55f87c0b9f0e9cc9e7f0
karolgil <karolgil.kg@gmail.com>
2015-01-11 21:04:29 +0100

    Missing cast at range.

--- UtilizationCalc/utilization_calc.py
+++ UtilizationCalc/utilization_calc.py
@@ -7,7 +7,7 @@ class UtilizationCalc(object):
 
     def calculate_vector_of_radiuses(self, delta, no_of_multicasts):
         radiuses = []
-        for m in range(1, no_of_multicasts):
+        for m in range(1, int(no_of_multicasts)):
             radiuses.append(round(math.sqrt(delta * delta + m * ((1 - delta * delta)/no_of_multicasts)), 5))
         radiuses.append(1)
         return radiuses
